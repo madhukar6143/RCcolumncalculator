c53faa325af4eaa3506a884137f6b30296a65ccd
madhukar6143 <madhukar.eppalapelly@gmail.com>
2022-01-03 19:00:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rccolumncalculator';
+  
+  Load:number
+  concreatestrength:number
+  gradeofsteel:number
+  percentageofsteel:number
+  effectivelength:number
+  grossarea:number
+  grossareaofconcreate:number
+  areaofsteel:number
+  areaofconcreate:number
+  grossareaofsteel:number
+  sizeofcolumn:number
+  numberofbars:number
+  diaofsteel:number=16
+  areaofonesteelbar:number=200.96
+  
+
+  rcColumnCalculator()
+  {
+    this.areaofsteel=this.percentageofsteel/100;
+   this.areaofconcreate=1-this.areaofsteel
+
+    this.grossarea=(this.Load*1000)/(0.4*this.concreatestrength*(this.areaofconcreate)+0.67*this.gradeofsteel*this.areaofsteel)
+
+    this.grossareaofconcreate=this.grossarea*this.areaofconcreate
+    this.grossareaofsteel=this.grossarea-this.grossareaofconcreate
+    this.sizeofcolumn=Math.sqrt(this.grossarea)
+    this.numberofbars=this.grossareaofsteel/this.areaofonesteelbar
+  }
 }
